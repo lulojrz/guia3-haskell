9620e9cf4dd1bf581d96ab3b979a15d9103a5b35
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-04-09 16:54:32 -0300

    Update test.hs

--- test.hs
+++ test.hs
@@ -25,10 +25,11 @@ absoluto:: Int -> Int
 absoluto x = abs x
 
 --2b 
-maxAbsoluto:: Int -> Int 
-maxAbsoluto x y | abs x > abs y   = x
-                | otherwise = y
-                  
+maxAbsoluto:: Int-> Int -> Int 
+maxAbsoluto x y     | abs x > abs y   = x
+                    | otherwise = y
+
+
 
 
 --2c
@@ -46,6 +47,9 @@ maximo :: Int-> Int -> Int -> Int
 maximo a b c | a>=b &&  a>= c = a
              | b>=a &&  b>= c = b
              | otherwise = c
+                  
+
+
 
 --2d
 {-- problema algunoEsCero (x:Q, y:Q) : Boolean {
@@ -55,14 +59,16 @@ maximo a b c | a>=b &&  a>= c = a
 --}
 
 -- Con Pattern Maching
-algunoEsCero :: (Int, Int) -> Bool
+algunoEsCero :: Int -> Int -> Bool
 algunoEsCero x y | x==0 || y==0   = True
                 | otherwise = False
 
 
 --Sin Pattern Maching
-algunoEsCero' :: (Int, Int) -> Bool
-algunoEsCero' (x, y) =  x==0 || y==0
+algunoEsCero' ::  Int -> Int -> Bool
+algunoEsCero' x y =  x==0 || y==0
+
+
 
 --2e 
 {--problema ambosSonCero (x: Q, y:Q) : Boolean{
@@ -71,7 +77,7 @@ algunoEsCero' (x, y) =  x==0 || y==0
 } --}
 
 -- Con Pattern Maching
-ambosSonCero :: (Int, Int) -> Bool
+ambosSonCero :: Int -> Int -> Bool
 ambosSonCero x y | x==0 && y==0   = True
                  | otherwise = False
 
@@ -79,7 +85,6 @@ ambosSonCero x y | x==0 && y==0   = True
 ambosSoncero1 :: (Int, Int) -> Bool
 ambosSoncero1(0,0) = True
 ambosSoncero1 (_ , _) = False
-
 
 
 --2f 
@@ -91,13 +96,11 @@ Los intervalos son : [-∞,3] [3,7] [7,∞]
 }
 
  --}
-
-enMismoIntervalo :: (Int,Int) -> Bool
-enMismoIntervalo (x,y) | (x <=3) && (y <=3) = True
+enMismoIntervalo :: Int -> Int -> Bool
+enMismoIntervalo x y   | (x <=3) && (y <=3) = True
                        | (x >3 && x<7 ) && (y > 3 && y < 7) = True
                        | (x >7) && (y >7) = True
                        | otherwise = False
-
 
 --2g
 {-- problema SumaDistintos(a:Z,b :Z, c:Z) :Z {
@@ -113,27 +116,18 @@ sumaDistintos a b c | a /= b &&  a/= c && b/=c  = a+b+c
                     | b==c && a /= b  = a
                     | a==b && b==c = 0
 
+
 --2h
 {--problema esMultiploDe (x:R, y:R): Boolean 
 requiere: true
 asegura: {si x es multiplo de  o viceversa, resultado sera true, de lo contrario sera False}
 --}
-esMultiploDe:: (Int,Int) -> Bool
-esMultiploDe (x,y) | y == 0  = False
+esMultiploDe::  Int -> Int -> Bool
+esMultiploDe  x y  | y == 0  = False
                    | mod x y == 0  = True
-                   |otherwise= False
+                   | otherwise= False
 
 
-
-
---2i
-{-- problema digitoUnidades (a:Z):Z {
-requiere: {true}
-asegura: dado un numero mayor o igual a 10 resultado es el ultimo digito
-asegura: en otro caso sera el mismo numero
-}--}
-digitoUnidades :: Int -> Int
-digitoUnidades n = mod(abs n ) 10
 
 
 --2j 
@@ -147,7 +141,216 @@ digitoDecenas :: Int -> Int
 digitoDecenas n =  digitoUnidades(div (abs n)  10)
 
 
+
+
+--2i
+{-- problema digitoUnidades (a:Z):Z {
+requiere {true}
+asegura: dado un numero mayor o igual a 10 resultado es el ultimo digito
+asegura: en otro caso sera el mismo numero
+}--}
+digitoUnidades :: Int -> Int
+digitoUnidades n = mod(abs n ) 10
+
 --3 
 
 
 
+
+
+
+
+
+--4a
+{-- problema productoInterno(x:(ZXZ), y: (ZXZ)):Z
+{
+requiere:true
+asegura: resultado sera el resultado de hacer la suma de los productos de la misma posicion
+}
+--}
+productoInterno:: (Int,Int) -> (Int,Int) -> Int
+productoInterno (a,b) (c,d) = (a*c) + (b*d)
+
+
+
+
+
+
+
+--4b 
+{-- problema todoMenor (x:(RXR),y:(RXR)): Bool
+ {
+ requiere : dos tuplas de numeros enteros de la misma cantidad de elementos
+
+ asegura: resultado sera true si cada coordenada de la primera tupla es menor a la misma coordenada de la otra tupla
+ }
+ --}
+
+todoMenor :: (Float, Float) -> (Float, Float) -> Bool
+todoMenor (x,y) (z,q) 
+                      | (z > x) && (q>y) = True
+                      | otherwise = False
+
+
+type Punto2D = (Float, Float)
+todoMenor1:: Punto2D -> Punto2D -> Bool
+todoMenor1 punto1 punto2 = (fst punto1 < (fst punto2)) && (snd punto1 < (snd punto2))
+
+--4c
+{--problema distanciaEuclideana (x:(ZXZ) y:(ZXZ) ): Z 
+{
+requiere: true
+asegura: resultado sera el resultado de hacer la distancia euclideana entre las dos tuplas
+
+}
+--}
+distanciaEuclideana::Punto2D -> Punto2D -> Float
+distanciaEuclideana (a,b) (c,d) = sqrt (((b-a)**2) + ((d-c)**2))
+
+
+
+
+
+--4d 
+{-- problema sumaTerna (x:(ZXZXZ)):Z{
+requiere: true
+asegura: resultado sera la suma de los tres elementos de la tupla
+
+} --}
+sumaTerna:: (Int,Int,Int) -> Int
+sumaTerna (x,y,z) = x+y+z
+
+--4e
+{-- problema sumarSoloMultiplos (x:(ZXZXZ), y: N ) : Z
+{
+requiere : true
+asegura: resultado sera la suma de los elementos de la tupla cuales sean multiplos del numero natural
+}
+
+--}
+sumarSoloMultiplos :: (Int,Int,Int) -> Int -> Int
+sumarSoloMultiplos (x,y,z) n | (mod x n == 0) &&  (mod y n == 0)  && (mod z n == 0) = x+y+z
+                             | (mod x n /=0) && (mod y  n== 0)  && (mod z n == 0) = y+z
+                             | (mod x n ==0) && (mod y  n/= 0)  && (mod z n == 0) = x+z
+                             | (mod x n ==0) && (mod y  n== 0)  && (mod z n /= 0) = x+y
+                             | (mod x n ==0) && (mod y  n/= 0)  && (mod z n /= 0) = x
+                            | (mod x n /=0) && (mod y  n== 0)  && (mod z n /= 0) = y
+                            | (mod x n /=0) && (mod y  n/= 0)  && (mod z n == 0) = z
+                         
+
+
+
+
+
+--4f
+{-- problema posPrimerPar(x:(ZXZXZ)): z posPrimerPar 
+requiere: true
+asegura: resultado sera 4 si todos los numeros de la terna son impares
+asegura: resultado sera la posicion del primer numero par encontrado, en el caso que existiese y usaremos del 1 al 3
+
+
+--}
+type Terna = (Int,Int,Int) 
+
+
+esPar :: Int -> Bool
+esPar numero = mod numero 2 == 0
+
+posPrimerPar :: Terna-> Int
+posPrimerPar (x,y,z) | esPar x = 1
+                     | esPar y = 2
+                     | esPar z = 3
+                     | otherwise = 4
+
+
+
+--4g
+{--problemacrearPar (x,y):(x,y){
+requiere : true
+asegura: resultado sera una tupla con los dos elementos ingresados
+} --}
+
+
+--4h
+{--problemaInvertirPar (x,y) : (y,x){
+requiere: true
+asegura: resultado sera la tupla de los dos elementos ingresados pero con el orden invertido
+} --}
+
+
+invertirPar:: a->   b -> (b,a) 
+invertirPar x y = (y,x)
+
+
+
+
+
+--5 
+
+--problema f (n : Z) : Z {
+--requiere: {True}
+--asegura: {(n ≤ 7 → res = n**2) ∧ (n > 7 → res = 2n − 1)}
+--}
+
+funcion:: Int -> Int
+funcion x | x <= 7 =  x^2
+          | x>7 = 2*x -1
+
+--problema g (n : Z) : Z {
+--requiere: {True}
+--asegura: {Si n es un numero par entonces res = n/2, en caso contrario, res = 3n + 1  }
+
+guncion :: Int -> Int
+guncion x  | mod x 2 == 0 =  div x 2
+           | otherwise = 3*x + 1
+
+
+--problema todosMenores (t : Z × Z × Z) : Bool {
+--requiere: {T rue}
+--asegura: {(res = true) ↔ ((f (t0) > g(t0)) ∧ (f (t1) > g(t1)) ∧ (f (t2) > g(t2)))}
+--}
+type TripleternaInt = (Int,Int,Int)
+todosMenores :: TripleternaInt -> Bool
+todosMenores (a,b,c) = funcion a > guncion a && funcion b > guncion b && funcion c > guncion c
+
+
+
+
+
+--6 
+type Anio = Int
+type EsBisiesto = Bool
+
+bisiesto :: Anio -> EsBisiesto
+bisiesto x | mod x 4 == 0 ||   mod x 100 == 0 ||  mod x 400 == 0 = True
+           | otherwise = False
+
+
+
+--7
+
+--distanciaManhattan:: (Float, Float, Float) -> (Float, Float, Float) -> Float
+--problema distanciaManhattan (p : R × R × R, q : R × R × R) : R {
+--requiere: {T rue}
+--asegura: {res = P2
+--i=0 |pi − qi|}
+--}
+--Por ejemplo:
+--distanciaManhattan (2, 3, 4) (7, 3, 8) ⇝ 9
+--distanciaManhattan ((-1), 0, (-8.5)) (3.3, 4, (-4)) ⇝ 12.8 ---
+type Tripleterna = (Float,Float,Float) 
+
+distanciaManhattan::Tripleterna -> Tripleterna -> Float
+
+distanciaManhattan (a,b,c) (d,e,f) = abs (a-d) + abs (b-e) + abs (c-f)
+
+
+
+--8
+sumarUltimoDosDigitos:: Int -> Int
+sumarUltimoDosDigitos x = mod (abs x ) 10 + mod (div (abs x) 10 ) 10
+
+comparar :: Int -> Int -> Int
+comparar a b   | sumarUltimoDosDigitos a > sumarUltimoDosDigitos b  = -1
+                | sumarUltimoDosDigitos b > sumarUltimoDosDigitos a = 1
+                | otherwise = 0 
