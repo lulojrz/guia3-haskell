34e8c232706a2fcfbdb18ece47a87c9175708b87
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-06-25 21:42:13 -0300

    Add files via upload

--- test.hs
+++ test.hs
@@ -1,147 +1,164 @@
-module GUIA where
---GUIA 3
---ejercicio 1
-f::Integer ->Integer
-f n | n==1 = 8
-    | n==4 = 131
-    | n==16 = 16
+module Test where
 
-g::Integer->Integer
-g n |n==8 = 16
-    |n==16 = 4
-    |n==131 =1
+import Test.HUnit
+import GUIA
 
-h::Integer->Integer
-h = f.g
-k::Integer->Integer
-k = g.f
+run = runTestTT testsumaultimodosdigitos
 
---ejercicio
-redondear::Float->Integer
-redondear n | n<0 = redondear(-n)
-            | n<1 = 0
-            | otherwise = 1 + redondear (n-1)
-      
---ejercicio 2a
-absoluto::(Num t, Ord t)=> t ->t
-absoluto n | n<0 = -n
-           | otherwise = n
+testfuncionf= test[
+    "caso 1:"~:(f 1)~?=8,
+    "caso 2:"~:(f 4)~?=131,
+    "caso 3:"~:(f 16)~?=16
+ ]
 
---ejercicio 2b
-maxAbsoluto::Int->Int->Int
-maxAbsoluto x y | absoluto x <absoluto y = absoluto y
-                | absoluto y <absoluto x = absoluto x
-                | otherwise = x
+testfunciong= test[
+    "caso 1:"~:(g 8)~?=16,
+    "caso 2:"~:(g 16)~?=4,
+    "caso 3:"~:(g 131)~?=1
+ ]
 
---ejercicio 2c
-maximo3::Int->Int->Int->Int
-maximo3 x y z | x>y && x > z =x
-              | y>x && y > z =y
-              | otherwise = z
+testfuncionh= test[
+    "caso 1:"~:(h 8)~?=16,
+    "caso 2:"~:(h 16)~?=131,
+    "caso 3:"~:(h 131)~?=8
 
---ejercicio 2d
-algunoesCero::Int->Int->Bool
-algunoesCero x y = (x==0) || (y==0)
+ ]
 
---ejercicio 2e
-ambosCero::Int->Int->Bool
-ambosCero x y = (x==0)&&(y==0)
+testfuncionk= test[
+    "caso 1:"~:(k 1)~?=16,
+    "caso 2:"~:(k 4)~?=1,
+    "caso 3:"~:(k 16)~?=4
+ ]
 
---ejercicio 2f
-mismoIntervalo::Int->Int->Bool
-mismoIntervalo x y | x <3 && y<3 = True
-                   | x>3 && x <7 && y>3 && y <7 = True
-                   | x>7 && y>7=True
-                   | otherwise = False
+testredondear= test[
+    "caso 1:"~:(redondear 67.4)~?=67,
+    "caso 2:"~:(redondear 3243.12544)~?=3243,
+    "caso 0:"~:(redondear 0.4)~?=0,
+    "caso negativo"~:(redondear (-4.4))~?=4
+ ]
 
---ejercicio 2g
-sumaDistintos::(Eq t , Num t)=>t->t->t->t
-sumaDistintos x y z | x/=y && y/=z && x/=z  =x+y+z
-                    | otherwise = 0
+testabsoluto = test[
+    "caso -:"~:(absoluto (-67))~?=67,
+    "caso +:"~:(absoluto 67)~?=67,
+    "caso 0:"~:(absoluto 0)~?=0
+ ]
 
---ejercicio 2h
-esMultiplode::Int->Int->Bool
-esMultiplode x y = mod x y == 0
+testmaximoabsoluto = test[
+    "caso 1"~:(maxAbsoluto (-34) 45)~?=45,
+    "caso 2"~:(maxAbsoluto (-67) 33)~?=67
+ ]
 
---ejercicio 2i
-digitoUnidades::Int->Int
-digitoUnidades x = mod x 10
+testmaximo3 = test[
+    "caso 1"~:(maximo3 1 56 77)~?=77,
+    "caso 2"~:(maximo3 1 560 77)~?=560,
+    "caso 3"~:(maximo3 100 56 77)~?=100,
+    "caso iguales"~:(maximo3 2 2 2)~?=2
+ ]
 
---ejercicio 2j 
-digitoDecenas::Int->Int
-digitoDecenas x = digitoUnidades (div x 10)
+testalgunoesCero= test[
+    "caso 1"~:(algunoesCero 3 2)~?=False,
+    "caso 2"~:(algunoesCero 0 2)~?=True,
+    "caso 3"~:(algunoesCero 1 0)~?=True,
+    "caso 4"~:(algunoesCero 0 0)~?=True
+ ]
 
+testAmbosCero = test[
+    "caso 1"~:(ambosCero 3 2)~?=False,
+    "caso 2"~:(ambosCero 0 2)~?=False,
+    "caso 3"~:(ambosCero 1 0)~?=False,
+    "caso 4"~:(ambosCero 0 0)~?=True
+ ]
 
---ejercicio 3
+testMismoIntervalo = test[
+    "caso 1"~:(mismoIntervalo (-1) 0)~?=True,
+    "caso 2"~:(mismoIntervalo 4 5)~?=True,
+    "caso 3"~:(mismoIntervalo 10 50)~?=True,
+    "caso 4"~:(mismoIntervalo (-4) 50)~?=False
+ ]
 
---ejercicio 4a
-productoInterno:: (Int,Int)->(Int,Int)->Int
-productoInterno (x,y) (z,u) = x*z + y*u
+testsumaDistintos = test[
+    "caso 1"~:(sumaDistintos 30 20 10)~?=60,
+    "caso repetidos"~:(sumaDistintos 3 3 3)~?=0
+ ]
 
---ejercicio 4b
-esParMenor::(Int,Int)->(Int,Int)->Bool
-esParMenor (x,y) (z,u) = x<z && y<u 
+testesMultiplode = test[
+    "caso 1"~:(esMultiplode (-60) 3)~?=True,
+    "caso 2"~:(esMultiplode 77 2)~?=False
+ ]
 
---ejercicio 4d
-sumaTerna::(Int,Int,Int)-> Int
-sumaTerna (x,y,z) = x+y+z
+testdigitounidades = test[
+    "caso 1"~:(digitoUnidades 30)~?=0,
+    "caso 2"~:(digitoUnidades 61)~?=1,
+    "caso 3"~:(digitoUnidades 3)~?=3
+ ]
 
---ejercicio 4e
-sumarSoloMultiplos :: (Int, Int, Int) -> Int -> Int
-sumarSoloMultiplos (x, y, z) n = sumar x (sumar y (sumar z 0))
-  where
-    sumar a b | mod a n == 0 = a + b
-              | otherwise    = b
+testdigitoDecenas = test[
+    "caso 1"~:(digitoDecenas 234)~?=3,
+    "caso 2"~:(digitoDecenas 302)~?=0
+ ]
 
---ejercicio 4f
-posPrimerPar:: [Int] -> Int
-posPrimerPar lista = buscar lista 0
-    where 
-    buscar [] _ = 4
-    buscar (y:ys) i | mod y 2 == 0 = i
-                    |otherwise = buscar ys (i+1)
+testproductointerno = test[
+    "caso 1"~:(productoInterno (3,4) (3,4))~?=25,
+    "caso 2"~:(productoInterno (1,1) (1,-1))~?=0
+  ]
 
+testesparMenor = test[
+    "caso true"~:(esParMenor (3,4) (6,7))~?=True,
+    "caso false"~:(esParMenor (30,40) (6,7))~?=False
+ ]
 
---ejercicio 4g
-crearPar::a->b->(a,b)
-crearPar x y = (x,y)
+testSumaTerna = test[
+    "caso suma correcta"~:(sumaTerna(3,3,3))~?=9
+ ]
 
---ejercicio 4h
-invertir:: (a,b)->(b,a)
-invertir (x,y) = (y,x)
+testSumarMultiplos= test[
+    "caso todos multiplos"~:(sumarSoloMultiplos(4,6,8) 2)~?=18,
+    "caso 2 multiplos"~:(sumarSoloMultiplos(3,8,10) 2)~?=18,
+    "caso 1 multiplo"~:(sumarSoloMultiplos(40,3,39) 13)~?=39,
+    "caso ningun multiplo"~:(sumarSoloMultiplos(13,13,13) 8)~?=0
+ ]
 
+testposPrimerPar = test [
+    "caso con pares"~:(posPrimerPar [3,5,10,5,5])~?=2,
+    "caso con impares"~:(posPrimerPar [3,5,5,5])~?=4
+ ]
 
---ejercicio 5
-todosMenores::(Integer,Integer,Integer)-> Bool
-todosMenores (x,y,z) = funcion x > guncion x && funcion y > guncion y && funcion z > guncion z
+testCrearpar = test[
+    "caso con enteros"~:(crearPar 5 6)~?=(5,6),
+    "caso con string"~:(crearPar "luca" "lulo")~?=("luca","lulo") 
+    ]
 
+testInvertir = test[
+    "caso con enteros"~:(invertir (3,5))~?=(5,3),
+    "caso con string"~:(invertir ("luca","lulo"))~?=("lulo","luca")    
+ ]
 
+testf = test[
+    "caso menor a 7"~:(funcion 5)~?=25,
+    "caso mayor a 7"~:(funcion 15)~?=29
+ ]
 
-funcion:: Integer-> Integer
-funcion n | n<=7 = n*n
-          | otherwise = 2*n -1
+testg = test[
+    "caso par"~:(guncion 6)~?=3,
+    "caso impar"~:(guncion 7 )~?=22
+ ]
 
-guncion::Integer-> Integer
-guncion n  | mod n 2 == 0 = div n 2
-           | otherwise = 3*n +1
+testTodosMenores = test[
+    "caso correcto"~:(todosMenores (4,6,7))~?=True
+ ]
+testbisiesto = test[
+    "caso 2000"~:(bisiesto 2000)~?=True,
+    "caso 1900"~:(bisiesto 1900)~?=False,
+    "caso 2100"~:(bisiesto 2100)~?=False
+ ]
 
+testDistanciaManhattan = test[
+    "caso consigna 1"~:(distanciaManhattan (2,3,4) (7,3,8))~?=9.0,
+    "caso consigna 2"~:(distanciaManhattan  ((-1), 0, (-8.5)) (3.3, 4, (-4)))~?=12.8
+ ]
 
-
---ejercicio 6
-bisiesto ::Integer -> Bool
-bisiesto n = (mod n 4 == 0) && ((mod n 100 /= 0) || (mod n 400 == 0))
-
---ejercicio 7
-distanciaManhattan:: (Float, Float, Float)-> (Float, Float, Float)-> Float
-distanciaManhattan (a,b,c) (d,e,f) = absoluto(a-d) + absoluto(b-e) + absoluto(c-f)
-
---ejercicio 8
-comparar::Integer-> Integer -> Integer
-comparar x y  | sumaUltimaDosDigitos x < sumaUltimaDosDigitos y = 1
-              | sumaUltimaDosDigitos y < sumaUltimaDosDigitos x = -1
-              | sumaUltimaDosDigitos x == sumaUltimaDosDigitos y = 0
-
-sumaUltimaDosDigitos::Integer->Integer
-sumaUltimaDosDigitos n = (mod n 10) + (mod (div n 10) 10 )
-
- 
+testsumaultimodosdigitos = test[
+    "caso -1"~:(comparar 45 312)~?=(-1),
+    "caso 1"~:(comparar 2312 7)~?=1,
+    "caso 0"~:(comparar 45 172)~?=0
+ ]
